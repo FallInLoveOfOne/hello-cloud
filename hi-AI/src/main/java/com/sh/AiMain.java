f0f72112eca8ca5238025fc2cfe3930c519eec04
su <lk19931990@163.com>
2025-07-21 23:53:12 +0800

    新增spring-ai-alibaba模块

---
+++ hi-AI/src/main/java/com/sh/AiMain.java
@@ -0,0 +1,13 @@
+package com.sh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiMain {
+    public static void main(String[] args) {
+        SpringApplication.run(AiMain.class, args);
+    }
+
+
+}
